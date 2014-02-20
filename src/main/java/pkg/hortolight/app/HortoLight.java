2933a31008d945228963dba5f20d66914b838b6d
James <james@TrueBlue.(none)>
2014-02-19 21:34:30 -0600

    Fixed permissions, features in manifest. Added SurfaceView, ViewHolder, and try catch. Syntax on Else statement.

--- src/main/java/pkg/hortolight/app/HortoLight.java
+++ src/main/java/pkg/hortolight/app/HortoLight.java
@@ -5,37 +5,52 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class HortoLight extends Activity {
 
     Button flashlightToggle;
+    SurfaceView camPlaceHolder;
     Camera cam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horto_light);
-        cam = Camera.open();
         flashlightToggle = (Button) findViewById(R.id.xmlButton);
+        camPlaceHolder = (SurfaceView) findViewById(R.id.surfaceView);
         flashlightToggle.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 if(flashlightToggle.getText().equals("OFF"))
                 {
-                    flashlightToggle.setText("ON");
-                    Camera.Parameters p = cam.getParameters();
-                    p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                    cam.setParameters(p);
-                    cam.startPreview();
+                    cam = Camera.open();
+                    try {
+                        cam.setPreviewDisplay(camPlaceHolder.getHolder());
+                        Camera.Parameters p = cam.getParameters();
+                        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                        cam.setParameters(p);
+                        cam.startPreview();
+                        flashlightToggle.setText("ON");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        cam.release();
+                        Toast.makeText(getApplicationContext(),"ERROR",Toast.LENGTH_LONG);
+                    }
+
                 }
 
-                else
-                    flashlightToggle.setText("OFF");
+                else{
                     cam.stopPreview();
                     cam.release();
+                    flashlightToggle.setText("OFF");
+                }
             }
         });
     }
